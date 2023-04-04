f8f0bc17d6ab6ed1544eb84ff1e08efa15e12ffb
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2023-04-04 15:00:04 -0400

    stats dict to html table

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,8 +1,53 @@
 import Table from "react-bootstrap/Table";
 
-const Stats = () => (
+const stats = [
+[{category: 'Favorite Foods', id: 1, food: 'Food 1', percentMatch: 95}, 
+{id: 2, food: 'Food 2', percentMatch: 90},
+{id: 3, food: 'Food 3', percentMatch: 85},
+],
+[{category: 'Least Favortie Foods', id: 1, food: 'Food 1', percentMatch: 5}, 
+{id: 2, food: 'Food 2', percentMatch: 10},
+{id: 3, food: 'Food 3', percentMatch: 15},
+],
+[{category: 'Favorite Cuisines', id: 1, food: 'Food 1', percentMatch: 95}, 
+{id: 2, food: 'Food 2', percentMatch: 90},
+{id: 3, food: 'Food 3', percentMatch: 85},
+],
+[{category: 'Least Favorite Cuisines', id: 1, food: 'Food 1', percentMatch: 5}, 
+{id: 2, food: 'Food 2', percentMatch: 10},
+{id: 3, food: 'Food 3', percentMatch: 15}
+]
+]
+
+function itemMap(x){
+  if (typeof x.category === 'undefined'){
+    return (
+      <tr key={x.id}>
+        <td>{x.food}</td>
+        <td>{x.percentMatch}</td>
+      </tr>
+    );
+  }
+  else {
+    return (
+      <tr key={x.id}>
+        <td rowspan={3}>{x.category}</td>
+        <td>{x.food}</td>
+        <td>{x.percentMatch}</td>
+      </tr>
+    );
+  }
+};
+
+export default function itemTable() {
+const statsTable1 = stats[0].map(itemMap)
+const statsTable2 = stats[1].map(itemMap)
+const statsTable3 = stats[2].map(itemMap)
+const statsTable4 = stats[3].map(itemMap)
+
+return (
   <div>
-    <h1>My stats</h1>
+    <h1>My Stats</h1>
     <Table bordered>
       <thead>
         <tr>
@@ -12,61 +57,12 @@ const Stats = () => (
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td rowSpan={3}>Favorite foods</td>
-          <td>Food 1</td>
-          <td>95%</td>
-        </tr>
-        <tr>
-          <td>Food 2</td>
-          <td>90%</td>
-        </tr>
-        <tr>
-          <td>Food 3</td>
-          <td>85%</td>
-        </tr>
-        <tr>
-          <td rowSpan={3}>Least favorite foods</td>
-          <td>Food 1</td>
-          <td>5%</td>
-        </tr>
-        <tr>
-          <td>Food 2</td>
-          <td>10%</td>
-        </tr>
-        <tr>
-          <td>Food 3</td>
-          <td>15%</td>
-        </tr>
-        <tr>
-          <td rowSpan={3}>Favorite cuisines</td>
-          <td>Cuisine 1</td>
-          <td>95%</td>
-        </tr>
-        <tr>
-          <td>Cuisine 2</td>
-          <td>90%</td>
-        </tr>
-        <tr>
-          <td>Cuisine 3</td>
-          <td>85%</td>
-        </tr>
-        <tr>
-          <td rowSpan={3}>Least favorite cuisines</td>
-          <td>Cuisine 1</td>
-          <td>5%</td>
-        </tr>
-        <tr>
-          <td>Cuisine 2</td>
-          <td>10%</td>
-        </tr>
-        <tr>
-          <td>Cuisine 3</td>
-          <td>15%</td>
-        </tr>
+        {statsTable1}
+        {statsTable2}
+        {statsTable3}
+        {statsTable4}
       </tbody>
     </Table>
   </div>
 );
-
-export default Stats;
+};
