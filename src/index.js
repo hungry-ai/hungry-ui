74eaa476be7aaa7aa51989f54f935622728cdc3a
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2023-04-28 17:02:12 -0400

    issue #6

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Admin from "./pages/Admin";
+import Upload from "./pages/Upload";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home />, errorElement: <PageNotFound /> },
   { path: "/admin", element: <Admin /> },
+  { path: "/upload", element: <Upload /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import Review from "../../components/Reviews/Review";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FaAt } from "react-icons/fa";
+import Image from "react-bootstrap/Image";
+
+const Upload = () => {
+  const imageCap = 10;
+  const [images, setImages] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
+
+  useEffect(() => {
+    if (images.length < 1 || images.length > imageCap) return;
+    const newImageUrls = [];
+    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
+    setImageURLs(newImageUrls);
+  }, [images]);
+
+  const onImageChange = (e) => {
+    setImages([...e.target.files]);
+  };
+
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Upload</h1>
+      <Form>
+        <InputGroup className="mb-3" controlId="formInsta">
+          <InputGroup.Text id="basic-addon1">
+            <FaAt />
+          </InputGroup.Text>
+          <Form.Control placeholder="instagram" style={{ maxWidth: 400 }} />
+        </InputGroup>
+
+        <Form.Group controlId="formFileMultiple" className="mb-3">
+          <Form.Label>Upload Multiple Images, (max.{imageCap}) </Form.Label>
+          <Form.Control
+            type="file"
+            multiple
+            accept="image/*"
+            onChange={onImageChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          {imageURLs.map((imageSrc) => (
+            <InputGroup className="mb-3" controlId="formImage">
+              <InputGroup.Text id="basic-addon1">
+                <div style={{ display: "block", width: 150, height: 150 }}>
+                  <Image src={imageSrc} thumbnail />
+                </div>
+              </InputGroup.Text>
+              <div
+                key={`inline-radio`}
+                className="mb-3"
+                style={{ padding: 30 }}
+              >
+                {["1", "2", "3", "4", "5"].map((rating) => (
+                  <Form.Check
+                    inline
+                    label={rating}
+                    name={`group-${imageSrc}`}
+                    type="radio"
+                    id={`inline-radio-${rating}`}
+                  />
+                ))}
+              </div>
+            </InputGroup>
+          ))}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Upload;
