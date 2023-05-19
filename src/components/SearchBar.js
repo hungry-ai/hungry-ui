811f39a9cb4cf5e5af936a09d47cfe35eb9151ca
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2023-05-19 15:41:38 -0400

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
-import { FaMapMarkerAlt, FaAt, FaSearch, FaPizzaSlice } from "react-icons/fa";
+import {
+  FaMapMarkerAlt,
+  FaAt,
+  FaSearch /*, FaPizzaSlice*/,
+} from "react-icons/fa";
 
 const SearchBar = ({ onSearch }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [find, setFind] = useState(searchParams.get("find"));
+  //const [find, setFind] = useState(searchParams.get("find"));
   const [location, setLocation] = useState(searchParams.get("location"));
   const [instagramUsername, setInstagramUsername] = useState(
     searchParams.get("instagramUsername")
   );
-
-  useEffect(() => {
-    if (find || location || instagramUsername)
-      onSearch(find, location, instagramUsername);
-  }, []);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -25,14 +24,14 @@ const SearchBar = ({ onSearch }) => {
     setSearchParams(
       Object.fromEntries(
         Object.entries({
-          find: find,
+          //find: find,
           location: location,
           instagramUsername: instagramUsername,
         }).filter(([_, v]) => v)
       )
     );
 
-    onSearch(find, location, instagramUsername);
+    onSearch(/*find, */ location, instagramUsername);
   };
 
   return (
