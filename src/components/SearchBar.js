13dc49dd4423a452c71c1c816deb41ed2cca38b3
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2023-05-29 13:03:52 -0400

    Zozo pre launch (#9)
    * Update SearchBar.js
    
    * Update SearchBar.js
    
    * bugfix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
-import { FaMapMarkerAlt, FaAt, FaSearch, FaPizzaSlice } from "react-icons/fa";
+import {
+  FaMapMarkerAlt,
+  FaAt,
+  FaSearch /*, FaPizzaSlice*/,
+} from "react-icons/fa";
 
 const SearchBar = ({ onSearch }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [find, setFind] = useState(searchParams.get("find"));
+  //const [find, setFind] = useState(searchParams.get("find"));
   const [location, setLocation] = useState(searchParams.get("location"));
   const [instagramUsername, setInstagramUsername] = useState(
     searchParams.get("instagramUsername")
   );
-
-  useEffect(() => {
-    if (find || location || instagramUsername)
-      onSearch(find, location, instagramUsername);
-  }, []);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -25,14 +24,14 @@ const SearchBar = ({ onSearch }) => {
     setSearchParams(
       Object.fromEntries(
         Object.entries({
-          find: find,
+          //find: find,
           location: location,
           instagramUsername: instagramUsername,
         }).filter(([_, v]) => v)
       )
     );
 
-    onSearch(find, location, instagramUsername);
+    onSearch(/*find, */ location, instagramUsername);
   };
 
   return (
@@ -40,6 +39,7 @@ const SearchBar = ({ onSearch }) => {
       <Navbar>
         <Navbar.Brand>Hungry AI</Navbar.Brand>
         <InputGroup>
+          {/*
           <InputGroup.Text>
             <FaPizzaSlice />
           </InputGroup.Text>
@@ -48,6 +48,7 @@ const SearchBar = ({ onSearch }) => {
             onChange={(e) => setFind(e.target.value)}
             defaultValue={searchParams.get("find")}
           />
+        */}
           <InputGroup.Text id="basic-addon1">
             <FaMapMarkerAlt />
           </InputGroup.Text>
@@ -56,6 +57,9 @@ const SearchBar = ({ onSearch }) => {
             style={{ maxWidth: 280 }}
             onChange={(e) => setLocation(e.target.value)}
             defaultValue={searchParams.get("location")}
+            pattern="[0-9]{5}"
+            title="Zipcode must be a 5-digit number."
+            required
           />
           <InputGroup.Text id="basic-addon1">
             <FaAt />
